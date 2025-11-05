d0c2a6341723513a531d2c4ddc29f2566d47fa79
Student-2024 <student-2024@ashalim.org>
2025-11-05 15:02:13 +0200

    init

---
+++ app/src/main/java/com/example/noamnakavfinal/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.noamnakavfinal;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+
+        });
+
+    }
+
+    public void login(View view) {
+        Intent go=new Intent(this,Register.class);
+        startActivity(go);
+    }
+
+    public void register(View view) {
+        Intent go=new Intent(this,Register.class);
+        startActivity(go);
+    }
+
+    public void about(View view) {
+        Intent go=new Intent(this,About.class);
+        startActivity(go);
+    }
+}
